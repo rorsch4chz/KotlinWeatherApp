f6cd6390bac9aa3f967a16053b036eb7081c2596
baizhou <422166210@qq.com>
2018-01-08 18:11:26 +0800

    delegate

---
+++ app/src/main/java/com/pretang/kotlinweatherapp/domain/CityForecastTable.kt
@@ -0,0 +1,24 @@
+package com.pretang.kotlinweatherapp.domain
+
+/**
+ * @author baizhou
+ * @data 2018/1/8
+ * @description
+ */
+object CityForecastTable {
+    val NAME = "CityForecast"
+    val ID = "_id"
+    val CITY = "city"
+    val COUNTRY = "country"
+}
+
+object DayForecastTable {
+    val NAME = "DayForecast"
+    val ID = "_id"
+    val DATE = "date"
+    val DESCRIPTION = "description"
+    val HIGH = "high"
+    val LOW = "low"
+    val ICON_URL = "iconUrl"
+    val CITY_ID = "cityId"
+}
